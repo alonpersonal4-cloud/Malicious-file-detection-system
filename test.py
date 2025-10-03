4149518c4125bb027ab80ef6dca8c8b86d6b2248
= <alonpersonal4@gmail.com>
2025-10-03 19:28:36 +0300

    Create test.py
    Created the test.py file and tested the check_file_size function

---
+++ test.py
@@ -0,0 +1,6 @@
+from functions import check_file_size
+#testing the check_file_size function
+test_file_size = check_file_size("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
+print(test_file_size)
+
+        
