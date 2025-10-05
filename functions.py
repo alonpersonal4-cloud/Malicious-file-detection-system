c1c724ab3fd3c2ef182b8c0d48eec9e1561391b4
= <alonpersonal4@gmail.com>
2025-10-05 16:25:03 +0300

    Update functions.py
    Upgraded identify_file_type
    Started  suspicious_strings

--- functions.py
+++ functions.py
@@ -10,6 +10,27 @@ def check_file_size(file_path):
     else:
         return None
 def identify_file_type(file_path):
-    extension = magic.Magic(mime=True).from_file(file_path)  
-    return extension
-    
+    path = Path(file_path)
+    if path.exists():
+        mime_type = magic.Magic(mime=True).from_file(path)  
+        return mime_type
+    else:
+        return None
+def suspicious_strings(file_path):
+    string_count = {}
+    string_list = ["system","shell","download","socket","encrypt","decrypt","payload"]
+    path = Path(file_path)
+    if path.exists():
+        with open(file_path, 'r') as f:
+            for line in f:
+                strings = line.strip().split()
+                for string in strings:
+                    lower_string = string.lower()
+                    if (lower_string in string_list):
+                        if lower_string in string_count:
+                            string_count[lower_string] += 1
+                        else:
+                            string_count[lower_string] = 1
+                return string_count
+            else :
+                return None                 
