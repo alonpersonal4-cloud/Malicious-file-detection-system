cba8d1d5ffb2b7fff3eeefa933bbac8b76797de8
= <alonpersonal4@gmail.com>
2025-10-05 15:51:35 +0300

    Update test
    tested identify_file_type function

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from functions import check_file_size
+import functions
 #testing the check_file_size function
-test_file_size = check_file_size("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
+test_file_size = functions.check_file_size("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
 print(test_file_size)
-
+#testing the identify_file_type function
+test_file_type = functions.identify_file_type("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
+print(test_file_type)
         
