bdf0a43d2bf7ad2e16e7fed1ec2af8b2145d0ebd
= <alonpersonal4@gmail.com>
2025-10-08 19:22:31 +0300

    Update test
    tested the 3rd function

--- test.py
+++ test.py
@@ -5,4 +5,9 @@ print(test_file_size)
 #testing the identify_file_type function
 test_file_type = functions.identify_file_type("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
 print(test_file_type)
+#testing the suspicious_strings function
+test_suspicious_strings1 = functions.suspicious_strings("C:/Users/user/Downloads/test.txt")
+test_suspicious_strings2 = functions.suspicious_strings("C:/Users/user/Downloads/emptytest.txt")
+test_suspicious_strings3 = functions.suspicious_strings("C:/Users/user/Downloads/binarytest.txt")
+print(test_suspicious_strings1 , test_suspicious_strings2 , test_suspicious_strings3)
         
