d60238f70968e9e6d08851067c0488ff53825ec9
= <alonpersonal4@gmail.com>
2025-10-10 15:59:34 +0300

    Update test
    fix the binary bug in suspicious_strings

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ print(test_file_type)
 test_suspicious_strings1 = functions.suspicious_strings("C:/Users/user/Downloads/test.txt")
 test_suspicious_strings2 = functions.suspicious_strings("C:/Users/user/Downloads/emptytest.txt")
 test_suspicious_strings3 = functions.suspicious_strings("C:/Users/user/Downloads/GitHubDesktopSetup-x64.exe")
-#print(test_suspicious_strings1 , test_suspicious_strings2 , test_suspicious_strings3)
-print(test_suspicious_strings3)
+print(test_suspicious_strings1 , test_suspicious_strings2 , test_suspicious_strings3)
+
         
