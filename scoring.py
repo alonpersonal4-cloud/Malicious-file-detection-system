029573cba747e5233acca4c32a76297bbebb870b
= <alonpersonal4@gmail.com>
2025-10-22 21:55:31 +0300

    Update scoring.py
    build entropy_score & suspicious_strings_score

--- scoring.py
+++ scoring.py
@@ -20,9 +20,12 @@ def size_score(file_path):
     return score , explanation
 def type_score(file_path):
     score =0
-    file_type = ""
     explanation=[]
     file_type = functions.identify_file_type(file_path)
+    if file_type is None:  
+        score = 0
+        explanation.append("Could not identify file type")
+        return score, explanation
     if "dosexec" in file_type  or "dll" in file_type :
         score += 2
         explanation.append("Executable File (DANGER)")
@@ -36,5 +39,60 @@ def type_score(file_path):
         score+=0
         explanation.append("Unknown file_type (proceed carefully)")
     return score , explanation        
+def entropy_score(file_path):
+    score = 0
+    explanation = []
+    file_entropy =functions.calculate_entropy(file_path)
+    if file_entropy is None:  
+        score =  0
+        explanation.append("Could not calculate the file's entropy")
+        return score, explanation
+    if file_entropy < 5:
+        score = 0
+        explanation.append("Very low entropy level (text)")
+    elif file_entropy < 6.5:
+        score +=1
+        explanation.append("Mid entropy level (high level language)")
+    elif file_entropy <7.5:
+        score+=2
+        explanation.append("High entropy level (ASCII character)")
+    else:
+        score+=3
+        explanation.append("Very high level of entropy (fully random)")
+    return score , explanation
+def suspicious_strings_score(file_path):
+    #string_list = ["system","shell","download","socket","encrypt","decrypt","payload"]
+    dangerous_words_list = ["system","shell","payload"]
+    threatening_words_list =["download","socket","encrypt","decrypt"]
+    dangerous_words= 0
+    threatening_words =0
+    score = 0
+    explanation = []
+    file_strings =functions.suspicious_strings(file_path)
+    if file_strings is None:  
+        score += 0
+        explanation.append("Could not find sispicious words in the file")
+    else:
+        for word in dangerous_words_list:
+            if dangerous_words_list[word] in file_strings:
+                dangerous_words+= file_strings[word]
+        if dangerous_words > 0:
+            explanation.append("Found the words '{}' in the file".format(dangerous_words_list))
+        for word2 in threatening_words_list:
+            if threatening_words_list[word2] in file_strings:
+                threatening_words+= file_strings[word2]
+        if threatening_words > 0:
+            explanation.append("Found a the words '{}' in the file".format(dangerous_words_list))
+    score = (dangerous_words * 0.5 ) + (threatening_words * 0.3)
+    if score < 0.5:
+        return 0, explanation
+    elif score  < 1.5:
+        return 1, explanation
+    elif score  < 2.5:
+        return 1.5, explanation
+    else :
+        return 2.5,explanation
+                
+        
     
     
